47c43ea033d9020d993a5e3917bd5f5b7d2c49d8
TaeWonn <xodns9485@gmail.com>
2021-01-26 23:10:47 +0900

    1.26 코딩 테스트

---
+++ src/year_2021/month_1/test_1_26/Test.java
@@ -0,0 +1,12 @@
+package year_2021.month_1.test_1_26;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(new Test().solution("+1234"));
+    }
+
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
