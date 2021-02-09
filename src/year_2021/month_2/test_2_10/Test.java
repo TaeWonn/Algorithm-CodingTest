ed82fb43f89ac6aa7a6e3bad65d7bba27ea9d776
TaeWonn <xodns9485@gmail.com>
2021-02-10 01:02:53 +0900

    2.10 코딩 테스트

---
+++ src/year_2021/month_2/test_2_10/Test.java
@@ -0,0 +1,18 @@
+package year_2021.month_2.test_2_10;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(new Test().solution(12));
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i = 1; i<= n ; i++) {
+            if(n % i == 0) answer += i;
+        }
+
+        return answer;
+    }
+}
